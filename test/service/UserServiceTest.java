56d831329d0c61f87564d64374350e5ef8b83dab
Rachid Aitbouzkri <Rachid Aitbouzkri@MeriemBARIZ>
2017-12-18 18:02:12 +0100

    tableau

--- test/service/UserServiceTest.java
+++ test/service/UserServiceTest.java
@@ -72,8 +72,8 @@ public class UserServiceTest {
     @Test
     public void testCreerUser() {
         System.out.println("creerUser");
-        String id = "youyou";
-        String password = "licence";
+        String id = "y";
+        String password = "me";
         UserService instance = new UserService();
         int expResult = 1;
         int result = instance.creerUser(id, password);
